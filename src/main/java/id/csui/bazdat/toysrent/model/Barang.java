dc69eb6ba3dbe25b5be960a5e180e29277f6721b
Bustomi Raharjo <bustomi.raharjo@tokopedia.com>
2019-05-31 23:31:42 +0700

    fix error mapping

--- src/main/java/id/csui/bazdat/toysrent/model/Barang.java
+++ src/main/java/id/csui/bazdat/toysrent/model/Barang.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @Builder
@@ -40,7 +37,9 @@ public class Barang {
     @Column(name = "warna")
     private String warna;
 
+    @Transient
     private Item item;
+    @Transient
     private User penyewa;
 
 }
